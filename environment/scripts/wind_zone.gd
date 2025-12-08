2fed26b1868f67cdf9f50fcbd727326cdb73700b
Doozitron9000 <Doozitron9000@gmail.com>
2025-12-09 00:49:33 +1300

    Updated windzone to include a stand in visual element
    This will later serve to display the particle shader on.

--- environment/scripts/wind_zone.gd
+++ environment/scripts/wind_zone.gd
@@ -4,11 +4,19 @@
 
 # To use this scene add it to a level and set its children to be editable
 # (Right click and enable editable children). THen resize the contained
-# collision as you see fit.
+# visuals as you see fit (the collsion will match these at run time).
 extends Area2D
+
+# get both the visual and collision elements
+@onready var visuals : Polygon2D = $Visuals
+@onready var collision : CollisionPolygon2D = $Collision
 
 # The vector representing the wind direction and magnitude
 @export var wind : Vector2
+
+func _ready() -> void:
+	# on ready match the collision polygon to the visual one
+	collision.polygon = visuals.polygon
 
 ## When a body enters this it has the wind applied to it
 ## if applicable
