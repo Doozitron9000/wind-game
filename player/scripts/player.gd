4d40a34ba0a9e0e554ce5e5c636b4cd7489c85a5
Doozitron9000 <Doozitron9000@gmail.com>
2025-12-08 18:58:50 +1300

    Added basic player and test level scene
    These are to serve as the basis for this
    weeks tasks. Feel free to remove them they
    are merely a suggestion.

---
+++ player/scripts/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var speed := 200 # the player's max speed
+var jump_velocity := -400 # the velocity imparted by the player jumping
+
+func _physics_process(delta: float) -> void:
+	movement(delta)
+
+## Handle the player's movement for this tick
+## [param delta] is the time (in seconds) since this was last called
+func movement(delta: float) -> void:
+	# get the player's movement axis
+	# this returns a value between -1 and 1 where -1 is maximally leftward and
+	# 1 is maximally rightward
+	var input_dir = Input.get_axis("left", "right")
+	
+	#=============================================
+	#===== locomotion goes here ==================
+	#=============================================
